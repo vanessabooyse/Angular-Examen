82637f70283a3f695eecac48c6d25744ffc36268
Vanessa Booyse <vanessabooyse@mbp-van-vanessa.alpaca.int>
2020-01-20 11:03:23 +0100

    addProducts

--- Angular-Examen/src/app/service/product.service.ts
+++ Angular-Examen/src/app/service/product.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ProductService {
 
    constructor() {}
+
+   
+
+    // Get all products
+    getAllProducts(): Product[] {
+      
+      let product1 = new Product("Iphone11", "Apple", "newest apple phone", 1000)
+      let product2 = new Product("Samsung10", "Samsung", "newest samsung phone", 900)
+
+      return new Array(product1, product2)
+      
+     
+   
+   }
 
 }
